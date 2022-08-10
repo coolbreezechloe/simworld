67801cf99f541283d32988db021fdc9f22b74d53
Chloe Beelby <CoolBreezeChloe@gmail.com>
2022-08-09 22:10:48 -0400

    Save surface

--- src/simworld/main.py
+++ src/simworld/main.py
@@ -3,6 +3,6 @@ from simworld.scenes.introduction import Introduction
 
 def start_game():
     pg.init()
-    pg.display.set_mode((800, 600))
+    surface = pg.display.set_mode((800, 600))
     Introduction(pg)
     

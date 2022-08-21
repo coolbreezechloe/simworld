cdadd1e9e07ee76ccb43d42ddbe1ce7903a0c010
Chloe Beelby <CoolBreezeChloe@gmail.com>
2022-08-21 12:57:35 -0400

    Remove invalid test case

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -4,14 +4,8 @@
 
 import unittest
 
-from simworld.main import add_one
-
-
-class TestSimple(unittest.TestCase):
-
-    def test_add_one(self):
-        self.assertEqual(add_one(5), 6)
-
+class TestTileMap(unittest.TestCase):
+    pass
 
 if __name__ == '__main__':
     unittest.main()

8a084fce3daedd6588dbe0924e9442cd8da60cee
Chloe Beelby <CoolBreezeChloe@gmail.com>
2022-09-04 19:35:45 -0400

    Ran autopep8

--- src/simworld/tile_set_browser.py
+++ src/simworld/tile_set_browser.py
@@ -7,6 +7,7 @@ from simworld.tileset import TileSet, get_tilesets
 
 log = logging.getLogger(__name__)
 
+
 class TileSetBrowser():
     def __init__(self, tileset: TileSet):
         self.tileset = tileset
@@ -100,25 +101,33 @@ class TileSetBrowser():
             for c in range(cols):
                 tile = self.tileset.tiles[(self.view_x + c, self.view_y + r)]
                 rec = tile.get_rect()
-                rec = rec.move([int(c*self.tileset.tile_width), int(r*self.tileset.tile_height)])
+                rec = rec.move([int(c*self.tileset.tile_width),
+                               int(r*self.tileset.tile_height)])
                 self.surface.blit(tile, rec)
                 if r == self.selected_row and c == self.selected_col:
-                    self.selected_tile = (self.view_x + self.selected_col, self.view_y + self.selected_row)
-                    rec2 = tile.get_rect().move([int(self.view_width*self.tileset.tile_width + self.tileset.tile_width), 0])
-                    tile2 = pygame.transform.scale(tile, (rec.width*2, rec.height*2))
+                    self.selected_tile = (
+                        self.view_x + self.selected_col, self.view_y + self.selected_row)
+                    rec2 = tile.get_rect().move(
+                        [int(self.view_width*self.tileset.tile_width + self.tileset.tile_width), 0])
+                    tile2 = pygame.transform.scale(
+                        tile, (rec.width*2, rec.height*2))
                     self.surface.blit(tile2, rec2)
-                    rec4 = rec2.move([int(self.tileset.tile_width * 2 + self.tileset.tile_width), 0])
-                    tile4 = pygame.transform.scale(tile, (rec.width*4, rec.height*4))
+                    rec4 = rec2.move(
+                        [int(self.tileset.tile_width * 2 + self.tileset.tile_width), 0])
+                    tile4 = pygame.transform.scale(
+                        tile, (rec.width*4, rec.height*4))
                     self.surface.blit(tile4, rec4)
         x0 = int(self.selected_col * self.tileset.tile_width)
         y0 = int(self.selected_row * self.tileset.tile_height)
-        pygame.draw.rect(self.surface, _white, (x0, y0, self.tileset.tile_width, self.tileset.tile_height), width=1)
+        pygame.draw.rect(self.surface, _white, (x0, y0,
+                         self.tileset.tile_width, self.tileset.tile_height), width=1)
         font = pygame.freetype.Font(None, size=25)
         if self.selected_tile:
             font.render_to(self.surface,
-            (self.tileset.tile_width*self.view_width + self.tileset.tile_width, self.tileset.tile_height*self.view_height + self.tileset.tile_height),
-            f"Selected Tile = {self.selected_tile}",
-            fgcolor=_white)
+                           (self.tileset.tile_width*self.view_width + self.tileset.tile_width,
+                            self.tileset.tile_height*self.view_height + self.tileset.tile_height),
+                           f"Selected Tile = {self.selected_tile}",
+                           fgcolor=_white)
 
     def run(self):
         clock = Clock()

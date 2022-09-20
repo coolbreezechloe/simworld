35a18bcb8edc04dc20bf33df5fe7c49b5e059d87
Chloe Beelby <CoolBreezeChloe@gmail.com>
2022-09-19 22:02:56 -0400

    Rename ProbabilitySpace to RuleEditor

--- src/simworld/rule_editor.py
+++ src/simworld/rule_editor.py
@@ -5,7 +5,7 @@ from pygame.time import Clock
 import logging
 import random
 
-from simworld.tileset import TileSet
+from simworld.tileset import TileSet, Tile
 from simworld.rules import Rules, load_rules
 from simworld.tileset_browser import load_tilesets
 
@@ -15,7 +15,7 @@ _black = (0, 0, 0)
 _white = (255, 255, 255)
 
 
-class ProbabilitySpace():
+class RuleEditor():
     def __init__(self, tileset: TileSet, rules: Rules):
         self.tileset = tileset
         self.width = 800
@@ -31,7 +31,7 @@ class ProbabilitySpace():
         surface = pygame.display.set_mode((self.width, self.height))
         log.debug(f"created surface: {surface}")
         pygame.display.set_caption(
-            f"Probability Space Reducer : {self.tileset.name}")
+            f"Rule Editor Reducer : {self.tileset.name}")
         self.surface = surface
 
     def _setup_state(self):
@@ -39,6 +39,11 @@ class ProbabilitySpace():
         self.quit = False
         self.view_x = 0
         self.view_y = 0
+        self.down_at = (0, 0)
+        self.up_at = (0, 0)
+        self.selected_row = 0
+        self.selected_col = 0
+        self.dirty = True
         self.map_width = 100
         self.map_height = 100
         self.tile_width = self.tileset.tile_width
@@ -66,27 +71,67 @@ class ProbabilitySpace():
                     break
                 elif event.type == pygame.KEYDOWN:
                     self._handle_keydown(event.key)
+                elif event.type == pygame.MOUSEMOTION:
+                    x, y = event.pos
+                    if not self.selected_col == x // self.tile_width or not self.selected_row == y // self.tile_height:
+                        self.selected_col = x // self.tile_width
+                        self.selected_row = y // self.tile_height
+                        self.dirty = True
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    x, y = event.pos
+                    self.down_at = ((x // self.tile_width) + self.view_x, (y // self.tile_height) + self.view_y)
+                elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    x, y = event.pos
+                    self.up_at = ((x // self.tile_width) + self.view_x, (y // self.tile_height) + self.view_y)
+                    if self.up_at == self.down_at:
+                        self._handle_click(*self.up_at)
+
+    def _handle_click(self, x: int, y: int) -> None:
+        options = self.current_state[(x, y)]
+        if len(options) == 1:
+            return
+        random.shuffle(options)
+        for o in options:
+            if self._fix(x, y, o):
+                return
+        log.error(f'No valid options for ({x}, {y}) in {self.current_state[(x, y)]}')
+        self.quit = True
+
+    def _fix(self, x: int, y: int, choice: Tile) -> bool:
+        """Attempt to reduce the entropy to zero (fix) a given choice at a given location (x, y)
+
+        This function will propogate the rules for the given choice and if the choice is valid
+        will return True and update the internal state. If the choice breaks a rule the function
+        returns False and the state is not modified"""
+        self.current_state[(x, y)] = [choice]
+        self.dirty = True
+        return True
 
     def _handle_keydown(self, key) -> None:
         def left():
             if self.view_x > 0:
                 self.view_x = self.view_x - 1
+                self.dirty = True
 
         def right():
             if self.view_x < self.map_width - self.view_width:
                 self.view_x = self.view_x + 1
+                self.dirty = True
 
         def up():
             if self.view_y > 0:
                 self.view_y = self.view_y - 1
+                self.dirty = True
 
         def down():
             if self.view_y < self.map_height - self.view_height:
                 self.view_y = self.view_y + 1
+                self.dirty = True
 
         def origin():
             self.view_x = 0
             self.view_y = 0
+            self.dirty = True
 
         def quit():
             self.quit = True
@@ -124,20 +169,20 @@ class ProbabilitySpace():
     def _get_game_surface(self) -> pygame.Surface:
         rows = self.view_height
         cols = self.view_width
-        surface = pygame.Surface(
-            (cols*self.tileset.tile_width, rows*self.tileset.tile_height))
+        surface = pygame.Surface((cols*self.tileset.tile_width, rows*self.tileset.tile_height))
         for r in range(rows):
             for c in range(cols):
-                pspace = self.current_state[(self.view_x + c, self.view_y + r)]
-                if len(pspace) == 1:
-                    tile = pspace[0]
-                    rec = tile[0].get_rect()
+                options = self.current_state[(self.view_x + c, self.view_y + r)]
+                if len(options) == 1:
+                    tile = options[0]
+                    rec = tile.get_rect()
                 else:
-                    font = pygame.freetype.SysFont(None, size=12)
-                    tile, rec = font.render(f"|S|={len(pspace)}", fgcolor=_white)
-                rec = rec.move([int(c*self.tile_width + 2), int(r*self.tile_height + 2)])
+                    font = pygame.freetype.SysFont(None, size=15)
+                    tile, rec = font.render(f"{len(options)}", fgcolor=_white)
+                rec = rec.move([int(c*self.tile_width), int(r*self.tile_height)])
                 surface.blit(tile, rec)
-
+        pygame.draw.rect(surface, _white, (self.selected_col*self.tile_width, self.selected_row *
+                         self.tile_height, self.tile_width, self.tile_height), width=1)
         pygame.draw.rect(
             surface, _white, (0, 0, surface.get_width(), surface.get_height()), width=1)
         return surface
@@ -145,9 +190,11 @@ class ProbabilitySpace():
     def run(self):
         clock = Clock()
         while self.quit is False:
-            self._update_screen()
-            pygame.display.flip()
-            clock.tick(60)
+            if self.dirty:
+                self._update_screen()
+                self.dirty = False
+                pygame.display.flip()
+                clock.tick(60)
             self._handle_events()
         log.debug('Quitting')
 
@@ -156,7 +203,7 @@ def show_all():
     tilesets = load_tilesets(pathlib.Path(pathlib.Path(__file__).parent.parent.parent / "assets").glob('*.png'))
     for rules in load_rules(pathlib.Path(pathlib.Path(__file__).parent.parent.parent / "assets").glob('*-rules.json')):
         tileset = list(filter(lambda _r: _r.name == rules.name, tilesets))[0]
-        t = ProbabilitySpace(tileset, rules)
+        t = RuleEditor(tileset, rules)
         t.run()
 
 

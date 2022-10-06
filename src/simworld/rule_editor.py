dab012ea9222d97772e0a64ead35cee51b76b64e
Chloe Beelby <CoolBreezeChloe@gmail.com>
2022-10-06 00:36:38 -0400

    Remove initial state

--- src/simworld/rule_editor.py
+++ src/simworld/rule_editor.py
@@ -17,7 +17,6 @@ log = logging.getLogger(__name__)
 
 _black = (0, 0, 0)
 _white = (255, 255, 255)
-_initial_state = {2, 3, 34, 35, 65, 66, 67, 97, 98, 99, 129, 130, 131, 161, 162, 163, 130, 429}
 
 ProbabilitySpace = dict[Coordinate, AvailableOptions]
 
@@ -41,7 +40,7 @@ class GlobalState():
     def reset_state(self) -> None:
         for c in range(self.map_width):
             for r in range(self.map_height):
-                self.current_state[(c, r)] = set(_initial_state)
+                self.current_state[(c, r)] = set()
 
     def get_state_by_coordinate(self, x: int, y: int) -> AvailableOptions:
         return self.current_state.get((x, y), set())

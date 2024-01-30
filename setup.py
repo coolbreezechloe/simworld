187eb73ec7194164db81a497bb1a049358f30943
Chloe Beelby <coolbreezechloe@gmail.com>
2024-01-29 21:29:05 -0500

    Update setup.py
    Add copyright

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 """A simple PyGame application
 
+simworld Copyright (C) 2024 Chloe Beelby
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+A copy of the GNU General Public License is included along with this program.
+See LICENSE.txt or on the internet at https://www.gnu.org/licenses/
+
+simworld Copyright (C) 2024 Chloe Beelby
+
 """
 
 # Always prefer setuptools over distutils
